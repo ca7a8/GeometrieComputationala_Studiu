61bc4d62340c64e373f08070e180f8cbe4fb74e2
catalin <rotariucatalin2001@gmail.com>
2022-03-23 08:58:48 +0200

    Add project files.

---
+++ TemaCercArieMinima/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemaCercArieMinima
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen albastru = new Pen(Color.Blue, 2), rosu = new Pen(Color.Red, 2);
+            Random rnd = new Random();
+
+            int n = 5; //rnd.Next(2, 5);
+            float raza = 1;
+            
+            float[] x_coordinate = new float[n];
+            float[] y_coordinate = new float[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                float x = rnd.Next(200, panel1.Width - 200);
+                float y = rnd.Next(100, panel1.Height - 100);
+                g.DrawEllipse(albastru, x - raza, y - raza, raza * 2, raza * 2);
+                x_coordinate[i] = x;
+                y_coordinate[i] = y;
+            }
+
+            float minx = x_coordinate.Min();
+            float maxx = x_coordinate.Max();
+            float miny = y_coordinate.Min();
+            float maxy = y_coordinate.Max();
+
+            float diametru = Math.Max((float)Math.Sqrt(Math.Pow(maxx - minx, 2)), (float)Math.Sqrt(Math.Pow(maxy - miny, 2)));
+            
+            //g.DrawEllipse(rosu, minx, miny, diametru, diametru);
+            //g.DrawRectangle(albastru, minx, miny, maxx - minx, maxy - miny);
+            //g.DrawEllipse(albastru, minx, miny, diametru, diametru);
+            g.DrawLine(rosu, minx, miny, maxx, maxy);
+            g.DrawLine(rosu, minx, maxy, maxx, miny);
+            //g.DrawLine(rosu, minx, miny, maxx, miny);
+        }
+    }
+}
---
+++ TemaConvexHull/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemaConvexHull
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Point> points = new List<Point>();
+        List<Point> varfuri = new List<Point>();
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            #region Random
+            Random rnd = new Random();
+            int n = rnd.Next(50);
+            #endregion
+            #region Pen
+            Pen randomPen = new Pen(Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)));
+            Pen bluePen = new Pen(Color.Blue, 3);
+            #endregion
+            #region Points Generator
+            for (int i = 0; i < n; i++)
+            {
+                Point p = new Point(rnd.Next(10, this.panel1.Width - 10), rnd.Next(10, this.panel1.Height - 10));
+                points.Add(p);
+                g.DrawEllipse(bluePen, p.X, p.Y, 2, 2);
+            }
+            #endregion
+
+
+        }
+    }
+}
